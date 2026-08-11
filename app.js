242eb165b50dec7d77ec35d218b16f453c9080a1
edwinwong851 <edwin.wong851@education.nsw.gov.au>
2026-08-11 20:20:33 +1000

    Update app.js
    Signed-off-by: edwinwong851 <edwin.wong851@education.nsw.gov.au>

--- app.js
+++ app.js
@@ -142,7 +142,7 @@ const IronCountdownEl = document.getElementById("IronCountdownValue");
 
 console.log("App.js loaded!");
 
-const stages = ["🌱", "🌿", "🌳", "🌲", "🌴"];
+const stages = ["🌱", "🌿", "🌳", "🌲", "🌴","💠","🪓😭"];
 
 // Tree species based on session length
 const species = [
