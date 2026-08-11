0c3d6f67f9484b8ea3401b611a975bf235b1f086
edwinwong851 <edwin.wong851@education.nsw.gov.au>
2026-08-11 18:05:00 +1000

    Update app.js
    Signed-off-by: edwinwong851 <edwin.wong851@education.nsw.gov.au>

--- app.js
+++ app.js
@@ -356,7 +356,7 @@ wood -= 50;
 essence -= 5;
 woodEl.textContent = wood;
 essenceEl.textContent = essence;
-forestEl.innerHTML += "<p>🏡 Treehouse crafted! Growth speed +0.5x</p>";
+forestEl.innerHTML += "<p>🏡 Treehouse crafted! Growth speed +0.75x</p>";
 increaseGrowth(0.5);
 saveForest();
 }
@@ -368,7 +368,7 @@ wood -= 80;
 essence -= 5;
 woodEl.textContent = wood;
 essenceEl.textContent = essence;
-forestEl.innerHTML += "<p>⛩️ Shrine crafted! Growth speed +2.0x</p>";
+forestEl.innerHTML += "<p>⛩️ Shrine crafted! Growth speed +2.5x</p>";
 increaseGrowth(2.0);
 saveForest();
 }
